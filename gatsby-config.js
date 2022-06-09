92c184041634548674fb5fb903e951f2072920c8
NM0038 <nm0038@NM0038.local>
2022-06-09 11:08:56 +0900

    UPDATE : added parameter to fix memory leak

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,6 @@ module.exports = {
     siteMetadata : {
         title: "Anki app",
         author: "dimitri@diverta.co",
-        axiosConfig : {
-            baseURL: 'https://kurokanji.g.kuroco.app/rcms-api',
-            credentials: true,
-            withCredentials: true
-        }       
     },
     plugins: [`gatsby-plugin-sass`,],
 }
