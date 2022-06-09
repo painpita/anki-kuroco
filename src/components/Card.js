1354a4743aaeb4b7edb958624011143fd43c38e1
painpita <dimitri@diverta.co.jp>
2022-06-09 17:47:24 +0900

    UPDATED : various style improvements

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,29 @@
 import React from "react"
 import "./card.css"
+import motion from "framer-motion"
+
 
 class Card extends React.Component {
     constructor(props){
       super()
     }
-  
-    render(){
+
+    styles = {}
+   render(){
       return <div className="card">
+      
       <div className="content">
         <div className="front">
           {this.props.card.ext_1}
         </div>
         <div className="back">
           <div className="meanings">
-          {this.props.card.ext_2}
+          {this.props.card.ext_2.split("\n").join("· ")}
           </div>
           <div className="pronunciations">
-            {this.props.card.ext_3}
-            {this.props.card.ext_4}
+            {this.props.card.ext_3.split("\n").join("· ")}
+            <br></br>
+            {this.props.card.ext_4.split("\n").join("· ")}
           </div>
         </div>
       </div>
