87929b265188ff1f7808b893eaa7d30831d99ad9
painpita <dimitri@diverta.co.jp>
2022-06-10 16:54:27 +0900

    UPDATED : improved display

--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -1,10 +1,11 @@
 import React from "react"
 import axios from "../../authAxios"
-import Card from "./Card"
-import "./card-displayer.scss"
+import "./card-details.scss"
 import { navigate } from "gatsby";
 import { Paper } from "@mui/material";
-class CardDisplayer extends React.Component {
+import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
+class CardDetail extends React.Component {
   constructor(props){
     super()
     this.state = {
@@ -13,7 +14,6 @@ class CardDisplayer extends React.Component {
   }
   async componentDidMount(){
     try{
-      console.log(this.props)
       let cardsReq = await axios.get("/4/card-detail/"+this.props.topics_id)
       await this.setState({card:cardsReq.data.details})
     }
@@ -23,22 +23,32 @@ class CardDisplayer extends React.Component {
   }
 
   render(){
-
-        const card = <div></div>
-    return <Paper 
-    sx={{
-        height: "500px",
-        backgroundColor: 'black',
+    return <Paper className={"paper"}
+      sx={{
+        backgroundColor:'black',
         color:'white'
       }}
     elevation={8}>
-    {this.state.card.ext_1}<br></br>
-    {this.state.card.ext_2}<br></br>
-    {this.state.card.ext_3}<br></br>
-    {this.state.card.ext_4}<br></br>
-
+    <Typography variant="" component="h1">
+    {this.state.card.ext_1}
+    </Typography>
+    <Typography variant="" component="h2">
+    Meanings : {this.state.card.ext_2}
+    </Typography>
+    <Typography variant="" component="p">
+    Onyomi readings : {this.state.card.ext_3}
+    </Typography>
+    <Typography variant="" component="p">
+    Kunyumi readings : {this.state.card.ext_4}
+    </Typography>
+    <Typography variant="" component="p">
+    Related words : {this.state.card.ext_6}
+    </Typography>
+    <Typography variant="" component="p">
+      <a target="_blank" rel="noreferrer" href={"https://jisho.org/search/%23kanji%20"+this.state.card.ext_1}><SearchIcon className='jishoLink'/></a>
+    </Typography>
     </Paper>
   }
 }
 
-export default CardDisplayer
+export default CardDetail
