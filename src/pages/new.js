8ee0196e1c60c6c827ae457f07d398da73495d31
painpita <dimitri@diverta.co.jp>
2022-06-10 16:54:41 +0900

    ADDED : support for new cards

---
+++ src/pages/new.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout"
+import NewCard from "../components/NewCard";
+const newPage = () => <div>
+  <Layout>
+    <NewCard></NewCard>
+  </Layout>
+  </div>
+
+export default newPage;
