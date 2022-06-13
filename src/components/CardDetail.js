7d5973c2342a039e225be7c72a79a3ed91b61ce9
painpita <dimitri@diverta.co.jp>
2022-06-13 18:42:49 +0900

    UPDATED : Added liking feature

--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -7,24 +7,41 @@ import Typography from '@mui/material/Typography';
 import SearchIcon from '@mui/icons-material/Search';
 import LikeButton from "./LikeButton"
 import { Box } from "@mui/material";
-
+import {Button} from "@mui/material";
 class CardDetail extends React.Component {
   constructor(props){
     super()
     this.state = {
-        card:{}
+        liked:false,
+        card:{},
+        connected : false
     }
   }
+
   async componentDidMount(){
+    let cardsReq = null;
     try{
-      let cardsReq = await axios.get("/4/card-detail/"+this.props.topics_id)
+      cardsReq = await axios.get("/4/card-detail/"+this.props.topics_id)
       await this.setState({card:cardsReq.data.details})
     }
     catch{
       navigate("/profile")
     }
-  }
+    try{
+      let favReq = await axios.get("6/fav")
+      let myLikes = favReq.data.list.map(like=>like.module_id)
+      if(myLikes.includes(cardsReq.data.details.topics_id)){
+        console.log("i have liked this card")
+        this.setState({liked:true})
+      }
+      this.setState({connected:true})
 
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+  
   render(){
     return <Paper className={"paper"}
       sx={{
@@ -48,11 +65,10 @@ class CardDetail extends React.Component {
     Related words : {this.state.card.ext_6}
     </Typography>
     <Box className="buttonsWrapper">
-    <LikeButton></LikeButton>
-
-      <Typography variant="" component="p">
-        <a target="_blank" rel="noreferrer" href={"https://jisho.org/search/%23kanji%20"+this.state.card.ext_1}><SearchIcon className='jishoLink'/></a>
-      </Typography>
+    <LikeButton connected={this.state.connected} liked={this.state.liked} topic={this.state}></LikeButton>
+    <Typography variant="" component="p" className='jishoLink'>
+      <a target="_blank" rel="noreferrer" href={"https://jisho.org/search/%23kanji%20"+this.state.card.ext_1}><SearchIcon/></a>
+    </Typography>
     </Box>
     </Paper>
   }
