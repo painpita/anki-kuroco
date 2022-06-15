bdf6ffbc77ec07dec01ff81b987f4a3dd31f3ceb
painpita <dimitri@diverta.co.jp>
2022-06-15 17:40:07 +0900

    ADDED : plugins for multi - language

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,22 @@ module.exports = {
           icon: 'src/favicon.png',
         },
       },
+      {
+        resolve: `gatsby-plugin-i18n`,
+        options: {
+          langKeyDefault: 'en',
+          langKeyForNull: 'en',
+          prefixDefault: false,
+          useLangKeyLayout: false,
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `markdown-pages`,
+          path: `${__dirname}/src/markdown-pages`,
+        },
+      },
+      `gatsby-transformer-remark`,
 ],
 }
