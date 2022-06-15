1fb12c8964b6b2c81f7a12ee8b917a6698d57aa6
painpita <dimitri@diverta.co.jp>
2022-06-15 17:44:23 +0900

    MISC

--- src/pages/card_details/[id].js
+++ src/pages/card_details/[id].js
@@ -2,7 +2,6 @@ import React from "react"
 import Layout from "../../components/Layout"
 import CardDetail from "../../components/CardDetail"
 const cardDetail = (location) => {
-  console.log(location.location.state)
   return <div>
   <Layout>
     <CardDetail myCard={location.location.state.myCard} topics_id={location.location.state.topics_id}></CardDetail>
