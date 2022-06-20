b5f95614ff6aea24720e23e9d218ae3ccca622a6
painpita <dimitri@diverta.co.jp>
2022-06-20 10:13:26 +0900

    NEW : ESM wrapper for node file

---
+++ gatsby-node.esm.js
@@ -0,0 +1,33 @@
+import { useIntl } from "gatsby-plugin-intl";
+
+exports.createPages = async ({ actions }) => {
+    // Use language iso for the routes
+    //const locale = intl.locale !== "en" ? `/${intl.locale}` : ""
+    const { createPage } = actions
+    /* if (page.path.match(/^\/.*\/card_details/)) {
+      // notice page.context.language
+      page.matchPath = `/${locale}/card_details/*`
+      createPage(page)
+    } */
+    createPage({
+      path: "/using-dsg",
+      component: require.resolve("./src/templates/using-dsg.js"),
+      context: {},
+      defer: true,
+    })
+  }
+  
+
+exports.onCreatePage = ({ page, actions }) => {
+    const { createPage, deletePage} = actions
+    // notice the addition of .*
+    const reg = new RegExp('\/(.*)\/(card_details)\/.*')
+    if (page.path.match(reg)){
+      deletePage(page)
+      console.log(page.path.match(reg))
+      // notice page.context.language
+      page.path = `/card_details/`
+      page.matchPath = `/card_details/:id`
+      createPage(page)
+    }
+  }
