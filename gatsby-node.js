9b5109b23084f2d57ae57d639b4b4f6ee0f8d3d6
painpita <dimitri@diverta.co.jp>
2022-06-22 11:12:46 +0900

    UPDATE : removed comments

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,20 +3,13 @@ const path = require("path")
 exports.onPostBuild = () => {
   console.log("Copying locales")
   fs.copySync(
-    path.join(__dirname, "/src/locales"),
+    path.join(__dirname, "/locales"),
     path.join(__dirname, "/public/locales")
   )
 }
 
 exports.createPages = async ({ actions }) => {
-    // Use language iso for the routes
-    //const locale = intl.locale !== "en" ? `/${intl.locale}` : ""
     const { createPage } = actions
-    /* if (page.path.match(/^\/.*\/card_details/)) {
-      // notice page.context.language
-      page.matchPath = `/${locale}/card_details/*`
-      createPage(page)
-    } */
     createPage({
       path: "/using-dsg",
       component: require.resolve("./src/templates/using-dsg.js"),
