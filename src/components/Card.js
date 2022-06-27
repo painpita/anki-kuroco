40113bd0860b8be653a2c691a8c13b35b1363e45
painpita <dimitri@diverta.co.jp>
2022-06-27 17:23:18 +0900

    MISC : indentation

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,22 +12,22 @@ const Card = (props) => {
       return(
       <Fade in={true} direction="up" mountOnEnter unmountOnExit {...(true ? { timeout: props.index*200 } : {})}>
       <div role="command" className="card" onClick={handleClick} onKeyDown={handleClick}>
-      <div className="content">
-        <div className="front">
-          {props.card.ext_1}
-        </div>
-        <div className="back">
-          <div className="meanings">
-          {props.card.ext_2.split("\n").join("· ")}
+        <div className="content">
+          <div className="front">
+            {props.card.ext_1}
           </div>
-          <div className="pronunciations">
-            {props.card.ext_3.split("\n").join("· ")}
-            <br></br>
-            {props.card.ext_4.split("\n").join("· ")}
+          <div className="back">
+            <div className="meanings">
+              {props.card.ext_2.split("\n").join("· ")}
+            </div>
+            <div className="pronunciations">
+              {props.card.ext_3.split("\n").join("· ")}
+              <br></br>
+              {props.card.ext_4.split("\n").join("· ")}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </Fade>
     )
   }
