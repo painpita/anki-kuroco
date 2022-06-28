0c393b094184d65e05568a51a3099bed3802c146
painpita <dimitri@diverta.co.jp>
2022-06-28 10:07:07 +0900

    UPDATE : increased timeout

--- cypress.config.js
+++ cypress.config.js
@@ -11,4 +11,12 @@ module.exports = {
       bundler: "webpack",
     },
   },
+  "retries": {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    "runMode": 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    "openMode": 0
+  }
 };
