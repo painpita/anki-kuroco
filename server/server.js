44775c75e0d20c94ad0c93c461d0aeab7572002d
painpita <dimitri.corre@outlook.fr>
2022-08-10 00:00:37 +0200

    UPDATE : display connected clients

--- server/server.js
+++ server/server.js
@@ -1,38 +1,41 @@
 const WebSocketServer = require('ws');
-const Game = require('./classes/Game.ts').Game;
+//const Game = require('./classes/Game.ts').Game;
 const wss = new WebSocketServer.Server({ port: 8081 });
 
 let clientList = {}
 
-const storeClients = (username, client) => {
+const storeClient = (username, client) => {
   clientList[username] = client;
 }
 
 const startGame = (userName1, client1, userName2, client2) => {
-  new Game(userName1, client1, userName2, client2);
+  //new Game(userName1, client1, userName2, client2);
 }
 
 const sendGameRequest = (userName2) => {
-  if(userName2 in clientList.keys){
-    console.log("sending request")
-    clientList[userName2].send('GAME_REQUEST');
+  if(userName2 in clientList.keys()){
+    clientList[userName2].send(JSON.stringify({type:'GAME_REQUEST', data:userName2}));
   }
 }
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
-    switch(data) {
+    JSONObject = JSON.parse(data);
+    switch(JSONObject.type) {
       case 'CONN' : 
-        ws.storeClients(data);
+        storeClient(JSONObject.data, ws);
         break;
       case 'GAME_REQUEST' :
-        sendGameRequest('blabla');
+        sendGameRequest(JSONObject.data);
         break;
       case 'GAME_ACCEPTED' :
         startGame();
         break;
+      case 'GET_CLIENTS':
+        ws.send(JSON.stringify({type:'CLIENT_LIST',data:Object.keys(clientList)}))
+        break;
     }    
   });
 
-  ws.send('Connection accepted');
+  ws.send(JSON.stringify({type:'INFO', data:'Connection accepted'}));
 });
--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -1,23 +1,38 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import {  useI18next } from "gatsby-plugin-react-i18next";
 import { isLoggedIn } from "../services/auth"
 
 const Challenge = (props) => {
 
     const [message, setMessage] = useState("")
+    const [clientList, setClientList] = useState([])
 
     const socket = new WebSocket('ws://localhost:8081');
 
-    socket.onopen = async function(e) {
+    useEffect(()=>{},[message,clientList]);
+    socket.onopen = async () =>  {
+      console.log("opening")
       let user = await isLoggedIn();
-      if(user) socket.send(user.email);
+      if(user){
+        socket.send(JSON.stringify({type:'CONN', data : user.email}))
+        getClients()
+      };
     };
     
     socket.onmessage = function(event) {
-      setMessage(event.data);
+      let JSONObject = JSON.parse(event.data);
+      switch(JSONObject.type){
+        case 'CLIENT_LIST' :
+            //setClientList(JSONObject.data);
+          break;
+        case 'INFO' :
+          setMessage(JSONObject.data);
+          break;
+      }
     };
     
     socket.onclose = function(event) {
+      console.log("on close")
       if (event.wasClean) {
         alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
       } else {
@@ -31,17 +46,26 @@ const Challenge = (props) => {
       alert(`[error] ${error.message}`);
     };
 
-    const sendGameRequest = () => {
-      socket.send("GAME_REQUEST");
+    const sendGameRequest = (userName2) => {
+      socket.send(JSON.stringify({type:"GAME_REQUEST", data:userName2}));
     }
 
+    const getClients = () => {
+      socket.send(JSON.stringify({type:'GET_CLIENTS'}))
+    }
+    
+    const displayClients = ''
     const t = useI18next()
       return(
       <div>Hello World !!
                <button onClick={sendGameRequest}>Send hello</button>
                <p>Message : {message}</p>
+               <ul>
+                {/* clientList.map((client)=><li key={client}>{client}</li>) */}
+              </ul>
       </div>
     )
+
   }
   
   export default Challenge
