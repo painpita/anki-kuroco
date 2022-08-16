c4ca88e9467e6568734e57e971ee51047f3769cf
Dimitri Corre <dimitri.corre@orange.com>
2022-08-16 17:48:35 +0200

    will be removed soon

--- server/classes/Game.ts
+++ server/classes/Game.ts
@@ -1,9 +1,8 @@
-import Player from './Player'
-export default class Game {
-
+export class Game {
     player1 : Player;
     player2 : Player;
     constructor(userName1, client1, userName2, client2){
+        console.log("launching new game !!!")
         this.player1 = new Player(userName1, client1);
         this.player2 = new Player(userName2, client2);
     }
